e44bfcd2c0a03befbb45afee0ada90ce3e977ecc
zoid-anurag <anurag.waliya14@gmail.com>
2019-12-28 01:12:16 +0530

    Second commit

---
+++ run.sh
@@ -0,0 +1,5 @@
+export TRAINING_DATA=input/train_folds.csv
+export FOLD=0
+export MODEL=$1
+
+python -m src.train
---
+++ src/dispatcher.py
@@ -0,0 +1,7 @@
+from sklearn import ensemble
+
+
+MODELS = {
+    'randomforest' : ensemble.RandomForestClassifier(n_estimators=200,n_jobs = -1, verbose=2),
+    'extratrees' : ensemble.ExtraTreesClassifier(n_estimators = 200, n_jobs =-1, verbose=2)
+}
